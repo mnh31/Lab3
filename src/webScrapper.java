2408cd4b1bf045331262f8486a7dd24f46aab809
mnh31 <42979832+mnh31@users.noreply.github.com>
2018-09-19 14:23:28 -0500

    Finished Lab commit

--- src/webScrapper.java
+++ src/webScrapper.java
@@ -1,5 +1,41 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class webScrapper {
-    public static void main (String[] unUsed){
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
+    public static void main (String[] unUsed) {
+        String wholeURL = urlToString("http://erdani.com/tdpl/hamlet.txt");
+        String allWords []  = wholeURL.split(" ");
+        int wordCount = allWords.length;
+        System.out.println("The URL contained " + wordCount + " words.");
+        String word = "prince";
+        int count = 0;
+        for (String i : allWords){
+            if(i.toLowerCase().contains(word)){
+                count++;
+            }
+        }
+        System.out.println("The URL contained " + word + " " + count + " times.");
 
     }
+
 }
